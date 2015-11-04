240b916dba5b25f42a33d089cd34ae3456caa797
matheuspoleza <matheus.poleza@contaazul.com>
2015-11-04 20:40:30 -0200

    Added bible api

--- theme/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from bible.models import Verse
-
-class Theme(models.Model):
-	name = models.CharField(max_length=128)
-	verses = models.ManyToManyField(Verse)
-
-	def __unicode__(self):
-		return self.name
--- theme/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from models import *
-
-class ThemeSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Theme
